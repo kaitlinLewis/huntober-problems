aee864e1b87d43f0b5aa811e2814d5c096528074
kaitlinLewis <kaitlinlewis.dev@gmail.com>
2022-10-06 14:20:38 -0400

    easy peasy

---
+++ week-1/day4.js
@@ -0,0 +1,5 @@
+//Given a string, reverse it. Any details like whitespace, letter casing, or punctuation should be preserved and flipped along with the rest of the string. For example:
+
+function flipString(string) {
+  return string.split("").reverse().join("");
+}

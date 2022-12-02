85e9c91df308591e766f8ca64fa8aae5d5a50364
kaitlinLewis <kaitlinlewis.dev@gmail.com>
2022-12-02 18:14:04 -0500

    standup

---
+++ standup/string_reversal.js
@@ -0,0 +1,17 @@
+// https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
+
+//Given a string of characters as input, write a function that returns it with the characters reversed. No Reverse Method (well brute force it first, but then no reverse method)!
+
+function reverseString(string) {
+  //return string.split("").reverse().join("")
+  //loop through characters and adding the character to the string
+  let reversedString = "";
+  for (let char of string) {
+    reversedString = char + reversedString;
+  }
+  return reversedString;
+}
+
+console.log(reverseString("brendan"), "nadnerb");
+console.log(reverseString("john"), "nhoj");
+console.log(reverseString("kaitlin"), "niltiak");
